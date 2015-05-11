778b03dde92edc2b1cdcd6419bb4f49dc10980d0
Solinari <justin.tyme.dejesus@gmail.com>
2015-05-11 12:47:58 -0500

    creating linear models and summaries for age

--- Age Scripts.R
+++ Age Scripts.R
@@ -1,6 +1,31 @@
 SQF.2012$AgeAboveNinetyNine <- factor ( with ( SQF.2012, ifelse ((age > 99),1, 0) ) );
 SQF.2012$AgeBelowOneHundred <- factor ( with ( SQF.2012, ifelse ((age < 100),1, 0) ) );
-m1 <- lm(frisked ~ SQF.2012$AgeBelowOneHundred, data=SQF.2012)
-m2 <- lm(frisked ~ SQF.2012$AgeAboveNinetyNine, data=SQF.2012)
-summary(m1)
-summary(m2)
+mLowFrisk <- lm(frisked ~ SQF.2012$AgeBelowOneHundred, data=SQF.2012)
+mHiFrisk <- lm(frisked ~ SQF.2012$AgeAboveNinetyNine, data=SQF.2012)
+mLowSearch <- lm(searched ~ SQF.2012$AgeBelowOneHundred, data=SQF.2012)
+mHiSearch <- lm(searched ~ SQF.2012$AgeAboveNinetyNine, data=SQF.2012)
+mLowContra <- lm(contrabn ~ SQF.2012$AgeBelowOneHundred, data=SQF.2012)
+mHiContra <- lm(contrabn ~ SQF.2012$AgeAboveNinetyNine, data=SQF.2012)
+mLowArrest <- lm(arstmade ~ SQF.2012$AgeBelowOneHundred, data=SQF.2012)
+mHiArrest <- lm(arstmade ~ SQF.2012$AgeAboveNinetyNine, data=SQF.2012)
+mLowSummon <- lm(sumissue ~ SQF.2012$AgeBelowOneHundred, data=SQF.2012)
+mHiSummon <- lm(sumissue ~ SQF.2012$AgeAboveNinetyNine, data=SQF.2012)
+
+attach(SQF.2012)
+SQF.2012$weaponfound <- pistol+riflshot+asltweap+knifcuti+machgun+othrweap
+
+mLowWeapon <- lm(SQF.2012$weaponfound ~ SQF.2012$AgeBelowOneHundred, data=SQF.2012)
+mHiWeapon <- lm(SQF.2012$weaponfound ~ SQF.2012$AgeAboveNinetyNine, data=SQF.2012)
+
+summary(mLowFrisk)
+summary(mHiFrisk)
+summary(mLowSearch)
+summary(mHiSearch)
+summary(mLowContra)
+summary(mHiContra)
+summary(mLowArrest)
+summary(mHiArrest)
+summary(mLowSummon)
+summary(mHiSummon)
+summary(mLowWeapon)
+summary(mHiWeapon)
